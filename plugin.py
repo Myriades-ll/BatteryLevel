33325511f8ca4293431a46d3fc2911200ba65534
Myriades-ll <myriades.ll@gmail.com>
2021-07-10 09:20:15 +0200

    Mise à jour plugin description

--- plugin.py
+++ plugin.py
@@ -16,7 +16,7 @@ Versions:
         <p>Version 1.0 for domoticz 2021.1 version</p>
         <h3>Options</h3>
         <h4>Empty level:</h4>
-        <p>The value from which you consider a battery empty. This value is also used for notification.</p><br/>
+        <p>The value from which you consider a battery empty. This value is also used for treshold notification and bounce system reset.</p><br/>
         <h4>Use every devices:</h4>
         <p>Automatically marks every device created by the plugin as used.</p><br/>
         <h4>Add notification:</h4>
